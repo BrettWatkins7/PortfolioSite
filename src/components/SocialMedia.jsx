7fe558a7839d7ea1600e5d64916cf39505531cdd
Brett Watkins <brettderek13147@gmail.com>
2022-12-12 13:47:51 -0600

    First Commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsTwitter, BsInstagram} from 'react-icons/bs';
+import {FaLinkedin} from 'react-icons/fa';
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+       
+        <div>
+        <a href='https://www.linkedin.com/in/brett-watkins-40a2a310a/'>
+            <FaLinkedin/>
+        </a>
+        </div>
+    </div>
+  )
+}
+
+export default SocialMedia
